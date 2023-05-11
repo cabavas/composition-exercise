4e1225ec104ba79f83932f8f5994d213c2b87bc4
cabavas <cabavas@gmail.com>
2023-05-11 18:33:36 -0300

    First commit

---
+++ src/main/java/org/example/entities/Department.java
@@ -0,0 +1,16 @@
+package org.example.entities;
+
+public class Department {
+    private String name;
+
+    public Department(String departmentName) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
